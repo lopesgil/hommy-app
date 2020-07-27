2c81c466699da5de63e884ac7f3a9f8b6458aed6
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2020-07-27 11:57:24 -0300

    # home reecbe dados por interpolação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  republica1: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.republica1 = {
+      img: '../../assets/img/republica1.jpg',
+      endereco: 'Vila dos Sonhos Quebrados, nº 219',
+      cidade: 'Cidade Maravilha',
+      bairro: 'Vila das Flores',
+      descricao: '4 quartos, 12 beliches, área de serviço ampla, salão de jogos, bem localizada próxima à estação de metro.',
+    };
   }
 
 }
